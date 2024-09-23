32689147cad4dfddbde8508859ad243e9ea41be0
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-09-23 11:55:46 +0530

    Learning JS

---
+++ JS/app.js
@@ -0,0 +1,84 @@
+// let c ='application';
+// if(c.length>3 && c[0]=='a'){
+//     console.log("Good String");
+// }
+// else{
+//     console.log("NA");
+// }
+
+// let newArr =['cars','maruti','audi'];
+// console.log(newArr);
+
+// for(let i=1; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+// const favMovie ="Avatar";
+// let guess =prompt("Guess the movie !")
+// while((guess!= favMovie) && (guess !="quit"))
+// {
+//     guess=prompt("Wrong ! try again.");
+// }
+// if(guess==favMovie){
+//     console.log("Correct!!!!!!");
+// }
+// else{
+//     console.log("Come Again!!!!!!!!!!");
+// }
+
+
+//  todo 
+
+// let todo=[];
+
+// let req=prompt("Enter Request....");
+
+// while(true){
+
+//     if(req=='quit'){
+//         console.log("Exit.");
+//         break;
+//     }
+
+//     if(req=='list'){
+//         console.log("----------------------");
+//         for(let i=0; i<=todo.length; i++){
+//              console.log(i, todo[i]);
+//         }
+//         console.log("----------------------");
+//     }
+
+//     else if(req=='add'){
+//         let task=prompt("enter todo:");
+//         todo.push(task);
+//         console.log("Todo Added");
+//     }
+
+//     else if(req=='delete'){
+//         let idx=prompt("enter index : ");
+//         todo.splice(idx,1);
+//         console.log("task deleted");
+//     }
+//     else{
+//         console.log("Wrong Request");
+//     }
+
+//     req=prompt("Enter new request..");
+// }
+
+
+// object litrals
+
+const studentInfo={
+    aman :{
+        grade :"A",
+        gender:"Male"
+    },
+    abhishek:{
+        grade:"C",
+        gender :"Male"
+    }
+};
+
+
+
